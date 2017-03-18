a3528bfe2ab70ed6192b8b315a863815696ac6e6
Sylvain GHERARDI <sylvain.gherardi96@live.fr>
2017-03-18 15:48:49 +0100

    Delete chainonpersonne.cpp

--- chainonpersonne.cpp
+++
@@ -1,9 +0,0 @@
-#include "chainonpersonne.h"
-#include <string>
-using namespace std;
-
-chainonPersonne::chainonPersonne(string nom, string prenom, string numero, string email):cp_nom{nom}, cp_prenom{prenom}, cp_numero{numero}, cp_email{email}
-{}
-
-chainonPersonne::~chainonPersonne()
-{}
